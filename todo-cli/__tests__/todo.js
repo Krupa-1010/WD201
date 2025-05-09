90bc63856dc99da77ad3c665464b868878d52317
Krupa Anna Kuruvilla <krupaannakuruvilla@gmail.com>
2025-05-09 22:45:48 +0530

    jest test added

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("Todo List test suit", () => {
   test("Should add new todo", () => {
@@ -19,5 +19,41 @@ describe("Todo List test suit", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("over due task correctly retrieved", () => {
+    add({
+      title: "overdue task ",
+      dueDate: "2025-05-08",
+      completed: false,
+    });
+
+    const overdueTasks = overdue();
+    expect(overdueTasks.length).toBe(1);
+    expect(all[1].dueDate).not.toBe(new Date().toISOString().slice(0, 10));
+  });
+
+  test("no-due task correctly retrieved", () => {
+    add({
+      title: "today task ",
+      dueDate: "2025-05-09",
+      completed: false,
+    });
+
+    const todayTasks = dueToday();
+    expect(todayTasks.length).toBe(2);
+    expect(all[2].dueDate).toBe(new Date().toISOString().slice(0, 10));
+  });
+
+  test("due later task correctly retrieved", () => {
+    add({
+      title: "due later task ",
+      dueDate: "2025-05-10",
+      completed: false,
+    });
+
+    const laterTasks = dueLater();
+    expect(laterTasks.length).toBe(1);
+    expect(all[3].dueDate).not.toBe(new Date().toISOString().slice(0, 10));
   });
 });
